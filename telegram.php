1083c96a9baabd480f5ed7bb67fcf6f72e0090c0
Vld1slav <103301051+Vld1slav@users.noreply.github.com>
2022-09-28 12:02:36 +0500

    1

--- telegram.php
+++ telegram.php
@@ -1,39 +1,68 @@
 <?php
+/* сбор инфы в секции */
+$zayavka=".";
+$marka = $_POST['marka'];
+$model = $_POST['model'];
+$problem = $_POST['problem'];
+$date = $_POST['date'];
+$probeg = $_POST['probeg'];
+$name = $_POST['name'];
+$last_name = $_POST['last_name'];
+$middle_name = $_POST['middle_name'];
+$tel=$_POST['tel'];
+$v1 = $_POST['v1'];
 
-//В переменную $token нужно вставить токен, который нам прислал @botFather
+
 $token = "5501594389:AAG0Mgv-_8pY3mQZb9_S0-kiphefSw8I7ss";
+$chat_id = "-748247926";
 
-//Сюда вставляем chat_id
-$chat_id = "-1010575349";
 
-//Определяем переменные для передачи данных из нашей формы
-if ($_POST['act'] == 'order') {
-    $name = ($_POST['name']);
-    $phone = ($_POST['phone']);
 
-//Собираем в массив то, что будет передаваться боту
-    $arr = array(
-        'Имя:' => $name,
-        'Телефон:' => $phone
-    );
+/* коды */
 
-//Настраиваем внешний вид сообщения в телеграме
-    foreach($arr as $key => $value) {
-        $txt .= "<b>".$key."</b> ".$value."%0A";
-    };
+$arr = array(
+  'ЗАЯВКА'=> $zayavka,
+  'Марка машины: ' => $marka,
+  'Модель: ' => $model,
+  'Описание проблемы:' => $problem,
+  'Год выпуска: ' => $date,
+  'Пробег: ' => $probeg,
+  'Фамилия: ' => $last_name,  
+  'Имя: ' => $name,
+  'Отчество: ' => $middle_name,
+  'Телефон:' => $tel,
+  'Вид связи:' => $v1,
+); 
 
-//Передаем данные боту
-    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-//Выводим сообщение об успешной отправке
-    if ($sendToTelegram) {
-        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
-    }
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
 
-//А здесь сообщение об ошибке при отправке
-    else {
-        alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
-    }
-}
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 ?>
+
+
+
+<?php
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+echo "Спасибо за заявку, ожидайте ответа!" ; 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <a href="index.html">Вернуться на страницу</a>
+</body>
+</html>
